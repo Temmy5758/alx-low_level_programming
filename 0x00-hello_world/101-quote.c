5663e89bd8a083a5db0a5364d95efa2b423aa9aa
Temmy5758 <temitopemosaku57@gmail.com>
2022-10-17 18:12:07 -0700

    ADV task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,2 @@
-#include <stdio.h>
-/**
-*main-entry point
-*Return:Always 0 (success)
-*/
-int main(void)
-{
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+#!/bin/bash
+gcc 101-quote.c -o

8659c84b7cfd7bd1b52f7e091b25a8aefcfab4af
Temmy5758 <temitopemosaku57@gmail.com>
2022-10-17 17:12:21 -0700

    my six task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main-entry point
+*Return:Always 0 (success)
+*/
+int main(void)
+{
+	printf("size of a char:%Id byte(s)\n", sizeof(char));
+	printf("size of an int:%Id byte(s)\n", sizeof(int));
+	printf("size of a long int:%Id byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%Id byte(s)\n", sizeof(long long int));
+	return (0);
+}
